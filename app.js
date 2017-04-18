caa589be481eaa89ae9eef47d207292cc351f615
gtop <gtop1985@gmail.com>
2017-04-18 15:26:48 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express          = require("express"),
+    app              = express(),
+    bodyParser       = require("body-parser"),
+    mongoose         = require("mongoose"),
+    flash            = require("connect-flash"),
+    passport         = require("passport"),
+    LocalStrategy    = require("passport-local"),
+    methodOverride   = require("method-override"),
+    Campground       = require("./models/campground"),
+    Comment          = require("./models/comment"),
+    User             = require("./models/user"),
+    seedDB           = require("./seeds");
+    
+    
+var commentRoutes    = require("./routes/comments"),
+    indexRoutes      = require("./routes/index"),
+    campgroundRoutes = require("./routes/campgrounds");
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.use(flash());
+seedDB();
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "top top top",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
---
+++ middleware/index.js
@@ -0,0 +1,57 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function (req, res, next) {
+     if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                req.flash("error", "Campground Not Found");
+                res.redirect("back");
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    req.flash("error", "You Don't Have Permission To Do That");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You Need To Be Logged In To Do That!");
+        res.redirect("back");
+    }
+    
+};
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You Don't Have Permission To Do That!");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You Need To Be Logged In To Do That!");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You Need To Be Logged In To Do That!");
+    res.redirect("/login");
+} ;
+
+module.exports = middlewareObj;
